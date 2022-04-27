d6945769946d438c1cc277caea17ecdb2a6cec5d
Can <77550017+cansati@users.noreply.github.com>
2022-04-27 12:08:24 +0300

    Smaller bitmap - Give pixel amount you wanna get and an image to produce it.

--- produceSmallerImageBitmapByAmountOfPixel.java
+++ produceSmallerImageBitmapByAmountOfPixel.java
@@ -1,4 +1,5 @@
 public Bitmap produceSmallerImageBitmapByAmountOfPixel(Bitmap image, int maxAmountOfPixel){
+        
         //2nd parameter is width x height
         int imageWidth = image.getWidth();
         int imageHeight = image.getHeight();
@@ -6,4 +7,5 @@ public Bitmap produceSmallerImageBitmapByAmountOfPixel(Bitmap image, int maxAmou
         int x = (int) Math.round(Math.sqrt(Math.round(imageRatio * maxAmountOfPixel)));
         int y = (int) Math.round(Math.sqrt(Math.round(maxAmountOfPixel / imageRatio)));
         return selectedImageBitmap.createScaledBitmap(image,x,y,true);
+        
     }
